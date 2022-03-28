a395f41f47c2352e26a47ec807d8acd45ce34126
Azad YILDIZ <azad.yildiz119@gmail.com>
2022-03-28 23:25:35 +0300

    Clean code & Add command line

--- dataController.js
+++ dataController.js
@@ -1,29 +1,29 @@
 const categoryList = []
-async function setCategoriesToCategoryList() {
+// Get usable data
+async function getCategories() {
     var data = await fetch("./product-list.json")
         .then(res => res.json())
         .then(data => data.responses[0][0].params.recommendedProducts)
         .catch(err => { console.log("error: " + err); })
     createCategoryObject(data)
 }
+// Create category object and push to array
 function createCategoryObject(data) {
-    var isActive = true
     var id = 0
-    Object.keys(data).forEach(element => {
-        var text = createCategoryText(element)
+    Object.keys(data).forEach(key => {
+        var text = createCategoryText(key)
         if (!text.includes("RECOMMENDATION")) {
             var category = {
                 "id": id,
                 "text": text,
-                "isActive": isActive,
-                "products": data[element]
+                "products": data[key]
             }
-            isActive = false
             id++
             categoryList.push(category)
         }
     })
 }
+// Get usable category text
 function createCategoryText(category) {
     if (category.includes(">"))
         var text = category.split(">")[0]
